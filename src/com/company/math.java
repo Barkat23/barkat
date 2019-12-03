8e2806298ad500014865675053e6eab51c1ac817
Barkat23 <barkatalirahimi.23@gmail.com>
2019-12-03 15:16:01 +0100

    new change

--- src/com/company/math.java
+++ src/com/company/math.java
@@ -4,7 +4,9 @@ public class math {
     public int add(int first, int second) {
         return first + second;
 }
-   
+
         public int sub(int first, int second) {
-            return first + second;
+            return first - first;
+}
+
 }
