2e72caaddc78121f4e7249ed03af8313bef21f70
Barkat23 <barkatalirahimi.23@gmail.com>
2019-12-03 14:23:42 +0100

    new sub method added

--- src/com/company/math.java
+++ src/com/company/math.java
@@ -4,4 +4,7 @@ public class math {
     public int add(int first, int second) {
         return first + second;
 }
+   
+        public int sub(int first, int second) {
+            return first + second;
 }
